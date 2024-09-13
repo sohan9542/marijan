de2409da2378de94496b45ab7e66add31877ccf8
Dev Sohan <sohanurrahmants@gmail.com>
2024-09-13 14:06:33 +0600

    dond

--- script.js
+++ script.js
@@ -12,6 +12,27 @@ document.addEventListener('DOMContentLoaded', function () {
             },
         }
     }).mount();
+
+
+
+});
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-slider2', {
+        "type": "loop",
+        perPage: 2,
+        perMove: 1,
+        gap: 10,
+
+        pagination: false,
+        breakpoints: {
+            600: {
+                perPage: 1,
+            },
+        }
+    }).mount();
+
+
+
 });
 
 let lastScrollTop = 0;
@@ -88,3 +109,92 @@ cartTrigger.addEventListener("click", (e) => {
 closeTrigger.addEventListener("click", (e) => {
     sidebar.style.transform = 'translateX(100%)'
 })
+
+
+
+
+let tab1Active = 0;
+const tab1 = document.querySelectorAll('.tab1');
+
+function updateBorders() {
+    tab1.forEach((item, index) => {
+        if (tab1Active === index) {
+            item.style.border = '2px solid black';
+        } else {
+            item.style.border = '2px solid transparent';
+        }
+    });
+}
+
+// Initialize the borders on page load
+updateBorders();
+
+// Add event listener to each button
+tab1.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        tab1Active = index;
+        updateBorders();  // Update all borders after clicking
+    });
+});
+
+
+let tab2Active = 0;
+const tab2 = document.querySelectorAll('.tab2');
+
+function updateBorders2() {
+    tab2.forEach((item, index) => {
+        if (tab2Active === index) {
+            item.style.border = '2px solid black';
+        } else {
+            item.style.border = '2px solid transparent';
+        }
+    });
+}
+
+// Initialize the borders on page load
+updateBorders2();
+
+// Add event listener to each button
+tab2.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        tab2Active = index;
+        updateBorders2();  // Update all borders after clicking
+    });
+});
+
+
+
+let dfaqActive = -1;
+const dfaq = document.querySelectorAll('.dfaq');
+
+function updateTab() {
+    dfaq.forEach((item, index) => {
+        const svgD = item.querySelector('svg');
+        const desc = item.querySelector('.desc');
+        desc.style.overflow = 'hidden'
+        if (dfaqActive === index) {
+            svgD.style.transform = 'rotate(0deg)'
+            desc.style.display = 'block'
+
+        } else {
+            svgD.style.transform = 'rotate(180deg)'
+             desc.style.display = 'none'
+        }
+    });
+}
+
+// Initialize the borders on page load
+updateTab();
+
+// Add event listener to each button
+dfaq.forEach((item, index) => {
+    item.addEventListener('click', () => {
+       if(dfaqActive !== index){
+        dfaqActive = index;
+       }
+       else{
+        dfaqActive = -1;
+       }
+        updateTab();  // Update all borders after clicking
+    });
+});
